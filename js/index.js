c861b911b5b5c87df0824320cb06d42042069994
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-01-08 09:09:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -88,4 +88,30 @@ billy.matchInterests(freeMusic);
 function(word) {
   return this.interests.includes(word);
 }
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        console.log("'this' is defined: ", this);
+        return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
 
